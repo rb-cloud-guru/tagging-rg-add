87141dc6bfedc8f06df1ce65d929828b3c64fbd9
Maroc <lznaidi@outlook.com>
2024-03-24 23:55:15 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,8 +8,8 @@ terraform {
 }
 provider "azurerm" {
     features {}
-    subscription_id = "24d682c1-836d-403d-8ee7-1719ffb8bcac"
-    client_id = "319ba94f-be24-4664-9320-e020548dd05b"
-    client_secret = "D478Q~YDycHxyc7AvKa-~f25Ffk89bjD_x1dXdpI"
-    tenant_id = "9bd4fb44-f0c3-42ce-8132-57bf4012a2f0"
+    subscription_id = ""
+    client_id = ""
+    client_secret = ""
+    tenant_id = ""
 }
